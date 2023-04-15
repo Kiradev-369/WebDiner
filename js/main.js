dc28ee7ef9797397c7e7498bba24f33f7b147afd
Kiradev <kira.hoangphong@gmail.com>
2023-04-15 15:23:58 +0700

    add new

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// * SEARCH
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//* NAVBAR MENU
+const navbarBtn = document.querySelector('.js-navbar-menu-btn')
+const overlay = document.querySelector('.js-overlay')
+const overlayClose = document.querySelector('.js-overlay-close')
+
+// * Hàm hiển thị overlay (thêm class 'open' vào overlay)
+function showNavbarMenu() {
+overlay.classList.add('open')
+}
+// * Hàm ẩn overlay ( gỡ bỏ class 'open' ra overlay)
+function hideNavbarMenu() {
+overlay.classList.remove('open')
+}
+// * nghe hành vi click vào navbarBtn 
+navbarBtn.addEventListener('click', showNavbarMenu)
+// * nghe hành vi click vào overlayclose 
+overlayClose.addEventListener('click',hideNavbarMenu )
+
+
+
+
+
+
+
