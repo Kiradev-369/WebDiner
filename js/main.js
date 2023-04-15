b7b4c6580d1fc43b795214d9da1d9bf653a9c4af
Kiradev <kira.hoangphong@gmail.com>
2023-04-15 18:12:14 +0700

    add themeDark

--- js/main.js
+++ js/main.js
@@ -1,19 +1,7 @@
-// * SEARCH
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// * LIGHT DARK MODE
+document.getElementById("switchButton").onclick = function () {
+    document.getElementById("myBody").classList.toggle("dark");
+};
 
 
 
@@ -23,26 +11,19 @@
 
 
 //* NAVBAR MENU
-const navbarBtn = document.querySelector('.js-navbar-menu-btn')
-const overlay = document.querySelector('.js-overlay')
-const overlayClose = document.querySelector('.js-overlay-close')
+const navbarBtn = document.querySelector(".js-navbar-menu-btn");
+const overlay = document.querySelector(".js-overlay");
+const overlayClose = document.querySelector(".js-overlay-close");
 
 // * Hàm hiển thị overlay (thêm class 'open' vào overlay)
 function showNavbarMenu() {
-overlay.classList.add('open')
+  overlay.classList.add("open");
 }
 // * Hàm ẩn overlay ( gỡ bỏ class 'open' ra overlay)
 function hideNavbarMenu() {
-overlay.classList.remove('open')
+  overlay.classList.remove("open");
 }
-// * nghe hành vi click vào navbarBtn 
-navbarBtn.addEventListener('click', showNavbarMenu)
-// * nghe hành vi click vào overlayclose 
-overlayClose.addEventListener('click',hideNavbarMenu )
-
-
-
-
-
-
-
+// * nghe hành vi click vào navbarBtn
+navbarBtn.addEventListener("click", showNavbarMenu);
+// * nghe hành vi click vào overlayclose
+overlayClose.addEventListener("click", hideNavbarMenu);
